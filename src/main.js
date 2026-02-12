6bc5245ee142f1bd63056cbcf0ac1fcc69fd2461
juniomenchik <juniomenchik@live.com>
2026-02-12 16:36:46 -0300

    vue to vercel. 0.0.1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Imprimir variável específica
+console.log('🌐 API URL:', import.meta.env.VITE_API_URL)
+
+// Imprimir todas as variáveis
+console.log('📦 Todas:', import.meta.env)
+
+// Verificar modo
+console.log('🔧 Modo:', import.meta.env.MODE)
+console.log('✅ Produção?', import.meta.env.PROD)
+
 createApp(App)
   .use(router)
   .mount('#app')
